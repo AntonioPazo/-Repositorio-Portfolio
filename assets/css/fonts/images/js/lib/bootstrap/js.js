70c0ca6be32ee91f79a692a585722481ae2c96e1
Antonio Pazo Porras <145558877+AntonioPazo@users.noreply.github.com>
2024-05-29 23:46:22 +0200

    Add files via upload

---
+++ assets/css/fonts/images/js/lib/bootstrap/js.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Nombre: " + "Antonio Pazo Porras" + "\nCorreo Electrónico: " + "pazoporras@hotmail.es" + "\nNúmero de Teléfono: " + "722637979" + "\nMensaje: " + "Ahora mismo la pagina no te envia el correo");
+});
